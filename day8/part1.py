4a3d24dac0caa69f03012090267e29027cc6bdae
nuruvilu <nuruvilu@konpaku.tech>
2021-12-08 00:40:13 -0600

    day 8

--- day8/part1.py
+++ day8/part1.py
@@ -1,8 +1,14 @@
 
 def solve(input_list: list):
-    pass
+    s = 0
+    for inp in input_list:
+        output = inp.split('|')[1].split()
+        for o in output:
+            if len(o) in {2, 3, 4, 7}:
+                s += 1
+    return s
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n')]
     print(solve(input_list))
