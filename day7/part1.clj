a42e38c87c2ca5e2d123a4745e3a308e580b1a50
nuruvilu <nuruvilu@konpaku.tech>
2021-12-07 22:17:14 -0600

    realize I can call Math/abs in clojure

--- day7/part1.clj
+++ day7/part1.clj
@@ -1,11 +1,9 @@
 
 (require '[clojure.string :as string])
 
-(defn dist [x y] (if (> x y) (- x y) (- y x)))
-
 (defn solve [input-list]
   (->> (range (apply min input-list) (inc (apply max input-list)))
-        (map #(reduce + (map (fn [x] (dist x %)) input-list)))
+        (map #(reduce + (map (fn [x] (Math/abs (- x %))) input-list)))
         (apply min)))
 
 (println (solve (map #(Integer/parseInt %)
--- day7/part2.clj
+++ day7/part2.clj
@@ -1,13 +1,11 @@
 
 (require '[clojure.string :as string])
 
-(defn dist [x y] (if (> x y) (- x y) (- y x)))
-
 (defn gauss [x] (/ (* x (inc x)) 2))
 
 (defn solve [input-list]
     (->> (range (apply min input-list) (inc (apply max input-list)))
-         (map #(reduce + (map (fn [x] (gauss (dist x %))) input-list)))
+         (map #(reduce + (map (fn [x] (gauss (Math/abs (- x %)))) input-list)))
          (apply min)))
 
 (println (solve (map #(Integer/parseInt %)
