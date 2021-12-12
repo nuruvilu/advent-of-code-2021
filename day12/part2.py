01b02d3e05d7d3836bb5cb1672f077678dd41bbd
nuruvilu <nuruvilu@konpaku.tech>
2021-12-12 00:04:33 -0600

    day 12

--- day12/part2.py
+++ day12/part2.py
@@ -1,8 +1,43 @@
+from functools import reduce
+import operator
+import copy
+
+cave_map = {}
+
+
+def explore(cave, past, firstsmall):
+    if cave not in past or not firstsmall:
+        past2 = copy.deepcopy(past)
+        if cave.islower():
+            if cave in past2:
+                firstsmall = cave
+            else:
+                past2.add(cave)
+        total = 0
+        for c2 in cave_map[cave]:
+            if c2 == 'end':
+                total += 1
+            elif c2 != 'start':
+                total += explore(c2, past2, firstsmall)
+        return total
+    return 0
+
 
 def solve(input_list: list):
-    pass
+    for inp in input_list:
+        p1, p2 = inp.split('-')
+        if p1 in cave_map:
+            cave_map[p1].append(p2)
+        else:
+            cave_map[p1] = [p2]
+        if p2 in cave_map:
+            cave_map[p2].append(p1)
+        else:
+            cave_map[p2] = [p1]
+    return reduce(operator.add,
+                  [explore(c, set(), None) for c in cave_map['start']])
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n')]
     print(solve(input_list))
