f66527728f5894046f4fb469685e1a24239e8fa9
nuruvilu <nuruvilu@konpaku.tech>
2021-12-14 00:24:27 -0600

    day 14

--- day14/part1.py
+++ day14/part1.py
@@ -1,8 +1,20 @@
+from collections import Counter
+
 
 def solve(input_list: list):
-    pass
+    template = input_list[0]
+    rules = [r.split(' -> ') for r in input_list[1].split('\n')]
+    rules = {r[0]: r[1] for r in rules}
+    for _ in range(10):
+        neo = template[0]
+        for i in range(1, len(template)):
+            inject = rules[template[i - 1: i + 1]]
+            neo += inject + template[i]
+        template = neo
+    counts = Counter(template)
+    return max(counts.values()) - min(counts.values())
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n\n')]
     print(solve(input_list))
--- day14/part2.py
+++ day14/part2.py
@@ -1,8 +1,25 @@
+from collections import defaultdict, Counter
+
 
 def solve(input_list: list):
-    pass
+    template = input_list[0]
+    rules = [r.split(' -> ') for r in input_list[1].split('\n')]
+    rules = {r[0]: r[1] for r in rules}
+    pairs = Counter(template[i - 1: i + 1] for i in range(1, len(template)))
+    for _ in range(40):
+        neo = defaultdict(int)
+        for p, c in pairs.items():
+            inject = rules[p]
+            neo[p[0] + inject] += c
+            neo[inject + p[1]] += c
+        pairs = neo
+    counts = defaultdict(int)
+    for p, c in pairs.items():
+        counts[p[0]] += c
+    counts[template[-1]] += 1
+    return max(counts.values()) - min(counts.values())
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n\n')]
     print(solve(input_list))
