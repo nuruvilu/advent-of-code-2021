371f19d9948b02fb8e8c497681d66af50bb7efe5
nuruvilu <nuruvilu@konpaku.tech>
2021-12-17 00:19:05 -0600

    day 17

--- day17/part2.py
+++ day17/part2.py
@@ -1,8 +1,37 @@
 
 def solve(input_list: list):
-    pass
+    front, back = input_list.split(', ')
+    ystr = back.split('=')[1]
+    miny, maxy = [int(x) for x in ystr.split('..')]
+    xstr = front.split('=')[1]
+    minx, maxx = [int(x) for x in xstr.split('..')]
+
+    def in_target(x, y):
+        return minx <= x <= maxx and miny <= y <= maxy
+
+    def shoot(x, y, dx, dy):
+        topy = -999999
+        i = 0
+        while not in_target(x, y) and i < 256:
+            x += dx
+            y += dy
+            if y > topy:
+                topy = y
+            if dx:
+                dx += 1 if dx < 0 else -1
+            dy -= 1
+            i += 1
+        return -999999 if i == 256 else topy
+
+    count = 0
+    for dx in range(1, maxx + 100):
+        for dy in range(miny - 100, 1000):
+            if shoot(0, 0, dx, dy) != -999999:
+                count += 1
+
+    return count
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = f.read().strip()
     print(solve(input_list))
