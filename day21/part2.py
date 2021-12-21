fdf831ae0a829bda0e33c6faf0cd4b43b7070fd9
nuruvilu <nuruvilu@konpaku.tech>
2021-12-21 00:38:34 -0600

    move sum calc out of loop

--- day21/part2.py
+++ day21/part2.py
@@ -12,19 +12,19 @@ def solve(input_list: list):
     p[(7, 2, 0, 0)] = 1
     turn = 0
     p1w, p2w = 0, 0
-    newu = list(product([1, 2, 3], [1, 2, 3], [1, 2, 3]))
+    newu = [sum(u) for u in product([1, 2, 3], [1, 2, 3], [1, 2, 3])]
     while p:
         newp = defaultdict(int)
         for (p1p, p2p, p1s, p2s), c in p.items():
             for uni in newu:
                 if not turn:
-                    p1p2 = ahh(p1p + sum(uni))
+                    p1p2 = ahh(p1p + uni)
                     if p1s + p1p2 >= 21:
                         p1w += c
                     else:
                         newp[(p1p2, p2p, p1s + p1p2, p2s)] += c
                 else:
-                    p2p2 = ahh(p2p + sum(uni))
+                    p2p2 = ahh(p2p + uni)
                     if p2s + p2p2 >= 21:
                         p2w += c
                     else:
