2d7358ad85db14f6cb49f8c13c485795c7586668
nuruvilu <nuruvilu@konpaku.tech>
2021-12-20 23:51:49 -0600

    day 21

--- day21/part1.py
+++ day21/part1.py
@@ -1,8 +1,45 @@
+import math
+from functools import lru_cache
+
+
+@lru_cache()
+def ahh(n):
+    while n > 10:
+        n -= 10
+    return n
+
+
+def next_die(n):
+    x = n
+    while True:
+        if x < 100:
+            x += 1
+        else:
+            x = 1
+        print(x)
+        yield x
+
 
 def solve(input_list: list):
-    pass
+    ps = [7, 2]
+    p = [0, 0]
+    dice = next_die(0)
+    turn = 0
+    rolls = 0
+    while p[0] < 1000 and p[1] < 1000:
+        roll = sum(next(dice) for _ in range(3))
+        print(roll)
+        newspot = ps[turn] + roll
+        ps[turn] = ahh(newspot)
+        print(ps)
+        p[turn] += ps[turn]
+        print(p)
+        turn = 0 if turn else 1
+        rolls += 3
+    print(p)
+    return min(p) * rolls
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
-    print(solve(input_list))
+    #input_list = [s for s in f.read().strip().split('\n')]
+    print(solve(None))
--- day21/part2.py
+++ day21/part2.py
@@ -1,8 +1,44 @@
+import math
+from functools import lru_cache
+from collections import defaultdict
+from itertools import product
+
+
+@lru_cache()
+def ahh(n):  # why even bother using your brain when u can do this???
+    while n > 10:
+        n -= 10
+    return n
+
 
 def solve(input_list: list):
-    pass
+    p = defaultdict(int)
+    p[(7, 2, 0, 0)] = 1
+    turn = 0
+    p1w, p2w = 0, 0
+    while p:
+        #print(len(p))
+        newp = defaultdict(int)
+        for (p1p, p2p, p1s, p2s), c in p.items():
+            newu = product([1, 2, 3], [1, 2, 3], [1, 2, 3])
+            for uni in newu:
+                if not turn:
+                    p1p2 = ahh(p1p + sum(uni))
+                    if p1s + p1p2 >= 21:
+                        p1w += c
+                    else:
+                        newp[(p1p2, p2p, p1s + p1p2, p2s)] += c
+                else:
+                    p2p2 = ahh(p2p + sum(uni))
+                    if p2s + p2p2 >= 21:
+                        p2w += c
+                    else:
+                        newp[(p1p, p2p2, p1s, p2s + p2p2)] += c
+        turn = 0 if turn else 1
+        p = newp
+    return max(p1w, p2w)
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
-    print(solve(input_list))
+    #input_list = [s for s in f.read().strip().split('\n')]
+    print(solve(None))
