1fa345a5474f875ebe425d36a9670067b283be66
nuruvilu <nuruvilu@konpaku.tech>
2021-12-22 22:34:54 -0600

    day 22

--- day22/part1.py
+++ day22/part1.py
@@ -1,8 +1,37 @@
+from collections import defaultdict
+
+
+ON = 0
+OFF = 1
+
 
 def solve(input_list: list):
-    pass
+    steps = []
+    for line in input_list:
+        groups = line.split(' ')[1].split(',')
+        t = ON if line[1] == 'n' else OFF
+        #print(groups)
+        groups = [group.split('=')[1].split('..') for group in groups]
+        #print(groups)
+        steps.append((t, [[int(n) for n in g] for g in groups]))
+    cubes = defaultdict(int)
+    for (t, ((minx, maxx), (miny, maxy), (minz, maxz))) in steps:
+        minx = max(-50, minx)
+        miny = max(-50, miny)
+        minz = max(-50, minz)
+        maxx = min(50, maxx)
+        maxy = min(50, maxy)
+        maxz = min(50, maxz)
+        for x in range(minx, maxx + 1):
+            for y in range(miny, maxy + 1):
+                for z in range(minz, maxz + 1):
+                    if t == ON:
+                        cubes[(x, y, z)] = 1
+                    else:
+                        cubes[(x, y, z)] = 0
+    return sum(cubes.values())
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n')]
     print(solve(input_list))
--- day22/part2.py
+++ day22/part2.py
@@ -1,8 +1,42 @@
+from functools import reduce
+from operator import mul
+
+
+ON = 1
+OFF = -1
+
+
+def intersect(c1, c2):
+    intersection = []
+    for i in range(3):
+        if c1[2 * i] > c2[2 * i + 1] or c1[2 * i + 1] < c2[2 * i]:
+            return False
+        intersection.append(max(c1[2 * i], c2[2 * i]))
+        intersection.append(min(c1[2 * i + 1], c2[2 * i + 1]))
+    return intersection
+
+
+def volume(c):
+    return reduce(mul, (c[2 * i + 1] - c[2 * i] + 1 for i in range(3)), 1)
+
 
 def solve(input_list: list):
-    pass
+    steps = []
+    for line in input_list:
+        groups = line.split(' ')[1].split(',')
+        t = ON if line[1] == 'n' else OFF
+        groups = [group.split('=')[1].split('..') for group in groups]
+        steps.append((t, [[int(n) for n in g] for g in groups]))
+    cubes = []
+    for (t, ((minx, maxx), (miny, maxy), (minz, maxz))) in steps:
+        curr = [minx, maxx, miny, maxy, minz, maxz]
+        cubes.extend([(intersect(cube, curr), -state)
+                      for cube, state in cubes if intersect(cube, curr)])
+        if t == ON:
+            cubes.append((curr, t))
+    return sum(volume(cube) * state for cube, state in cubes)
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n')]
     print(solve(input_list))
