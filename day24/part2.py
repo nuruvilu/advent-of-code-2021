0eaffe1e7ceaf44420b8bdb560d2ada4ea0b8a38
nuruvilu <nuruvilu@konpaku.tech>
2021-12-24 02:14:03 -0600

    days 23 and 24 :^)

--- day24/part2.py
+++ day24/part2.py
@@ -1,8 +1,8 @@
 
 def solve(input_list: list):
-    pass
+    n3 + 7 - 10 = n4
 
 
 with open('input.txt', 'r') as f:
-    input_list = [s.strip('\n') for s in f.readlines()]
+    input_list = [s for s in f.read().strip().split('\n')]
     print(solve(input_list))
