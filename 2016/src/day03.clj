1fb7566709674a4d919465959c73204106594b1e
nuruvilu <nuruvilu@konpaku.tech>
2022-01-14 02:22:09 -0600

    touch up day 3 2016

--- 2016/src/day03.clj
+++ 2016/src/day03.clj
@@ -3,15 +3,14 @@
 (defn triangle? [[A B C]] (> (- (+ A B C) (max A B C)) (max A B C)))
 
 (defn parse [input-text]
-  (->> (string/split-lines input-text)
-       (map #(map (fn [s] (Integer/parseInt s))
-                  (string/split (string/trim %) #"\s+")))))
+  (map #(map (fn [s] (Integer/parseInt s))
+             (string/split (string/trim %) #"\s+"))
+       (string/split-lines input-text)))
 
-(defn rotate [[A1 B1 C1] [A2 B2 C2] [A3 B3 C3]]
+(defn rotate [[[A1 B1 C1] [A2 B2 C2] [A3 B3 C3]]]
   [[A1 A2 A3] [B1 B2 B3] [C1 C2 C3]])
 
-(defn pivot [triangles]
-  (mapcat #(apply rotate %) (partition 3 triangles)))
+(defn pivot [triangles] (mapcat rotate (partition 3 triangles)))
 
 (defn part1 [input] (count (filter triangle? (parse input))))
 
