acf68678847dd721ce1a4441679fa847e6062e90
nuruvilu <nuruvilu@konpaku.tech>
2022-12-05 01:38:17 -0600

    2022 day 5

--- 2022/src/day05/part1.py
+++ 2022/src/day05/part1.py
@@ -8,12 +8,40 @@ from heapq import heappush, heappop
 from functools import reduce
 
 import common as aoc
+import re
+
+
+stacks = [
+    ['L', 'C', 'G', 'M', 'Q'],
+    ['G', 'H', 'F', 'T', 'C', 'L', 'D', 'R'],
+    ['R', 'W', 'T', 'M', 'N', 'F', 'J', 'V'],
+    ['P', 'Q', 'V', 'D', 'F', 'J'],
+    ['T', 'B', 'L', 'S', 'M', 'F', 'N'],
+    ['P', 'D', 'C', 'H', 'V', 'N', 'R'],
+    ['T', 'C', 'H'],
+    ['P', 'H', 'N', 'Z', 'V', 'J', 'S', 'G'],
+    ['G', 'H', 'F', 'Z']
+]
+
+stacks = [
+    ['N', 'Z'],
+    ['D', 'C', 'M'],
+    ['P']
+]
 
 
 def solve(inp):
-    pass
+    for stack in stacks:
+        stack.reverse()
+    for line in inp:
+        line = line[5:]
+        print(re.split(r'(\sfrom\s|\sto\s)', line))
+        cnt, _, src, _, dst = re.split(r'(\sfrom\s|\sto\s)', line)
+        for _ in range(int(cnt)):
+            stacks[int(dst) - 1].append(stacks[int(src) - 1].pop())
+    return ''.join([s[-1] for s in stacks])
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('sample.txt')
     print(solve(inp))
--- 2022/src/day05/part2.py
+++ 2022/src/day05/part2.py
@@ -8,12 +8,42 @@ from heapq import heappush, heappop
 from functools import reduce
 
 import common as aoc
+import re
+
+
+stacks = [
+    ['L', 'C', 'G', 'M', 'Q'],
+    ['G', 'H', 'F', 'T', 'C', 'L', 'D', 'R'],
+    ['R', 'W', 'T', 'M', 'N', 'F', 'J', 'V'],
+    ['P', 'Q', 'V', 'D', 'F', 'J'],
+    ['T', 'B', 'L', 'S', 'M', 'F', 'N'],
+    ['P', 'D', 'C', 'H', 'V', 'N', 'R'],
+    ['T', 'C', 'H'],
+    ['P', 'H', 'N', 'Z', 'V', 'J', 'S', 'G'],
+    ['G', 'H', 'F', 'Z']
+]
+
+# stacks = [
+#     ['N', 'Z'],
+#     ['D', 'C', 'M'],
+#     ['P']
+# ]
 
 
 def solve(inp):
-    pass
+    for stack in stacks:
+        stack.reverse()
+    for line in inp:
+        line = line[5:]
+        #print(re.split(r'(\sfrom\s|\sto\s)', line))
+        cnt, _, src, _, dst = re.split(r'(\sfrom\s|\sto\s)', line)
+        source = stacks[int(src) - 1]
+        for e in source[len(source) - int(cnt):]:
+            source.pop()
+            stacks[int(dst) - 1].append(e)
+    return ''.join([s[-1] for s in stacks])
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
