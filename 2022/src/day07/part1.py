5ddde17ff142cecc21a35d65a79e268d5fbc56f8
nuruvilu <nuruvilu@konpaku.tech>
2022-12-07 00:43:34 -0600

    2022 day 7

--- 2022/src/day07/part1.py
+++ 2022/src/day07/part1.py
@@ -9,11 +9,51 @@ from functools import reduce
 
 import common as aoc
 
+s = 0
+def sum_sizes(tree):
+    if isinstance(tree, int):
+        return tree
+    else:
+        global s
+        s2 = 0
+        for t in tree.values():
+            s2 += sum_sizes(t)
+        if s2 <= 100_000:
+            #print('yes', tree, s, s2)
+            s += s2
+        else:
+            #print('no', tree)
+            pass
+        return s2
+
 
 def solve(inp):
-    pass
+    dir_tree = {}
+    curr = dir_tree
+    parents = []
+    for line in inp[1:]:
+        head, tail = line.split(' ', maxsplit=1)
+        if head == '$':
+            if ' ' in tail:
+                _cmd, dst = tail.split()
+                if dst == '..':
+                    curr = dir_tree
+                    parents.pop()
+                    for parent in parents:
+                        curr = curr[parent]
+                else:
+                    parents.append(dst)
+                    curr[dst] = {}
+                    curr = curr[dst]
+        elif head == 'dir':
+            pass
+        else:
+            curr[tail] = int(head)
+    print(dir_tree)
+    sum_sizes(dir_tree)
+    return s
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
--- 2022/src/day07/part2.py
+++ 2022/src/day07/part2.py
@@ -1,5 +1,6 @@
 import math
 import operator
+import json
 
 import sys
 sys.path.append('..')
@@ -10,10 +11,50 @@ from functools import reduce
 import common as aoc
 
 
+dirs = []
+def sum_sizes(tree):
+    if isinstance(tree, int):
+        return tree
+    else:
+        s2 = 0
+        for t in tree.values():
+            s2 += sum_sizes(t)
+        dirs.append(s2)
+        return s2
+
+
 def solve(inp):
-    pass
+    dir_tree = {}
+    curr = dir_tree
+    parents = []
+    for line in inp[1:]:
+        head, tail = line.split(' ', maxsplit=1)
+        if head == '$':
+            if ' ' in tail:
+                _cmd, dst = tail.split()
+                if dst == '..':
+                    curr = dir_tree
+                    parents.pop()
+                    for parent in parents:
+                        curr = curr[parent]
+                else:
+                    parents.append(dst)
+                    curr[dst] = {}
+                    curr = curr[dst]
+        elif head == 'dir':
+            pass
+        else:
+            curr[tail] = int(head)
+    #print(json.dumps(dir_tree, indent=2))
+    print(sum_sizes(dir_tree))
+    total = max(dirs)
+    space_free = 70_000_000 - total
+    target = 30_000_000 - space_free
+    print(total, target)
+    #print(sorted(dirs))
+    return min([d for d in dirs if d >= target])
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
