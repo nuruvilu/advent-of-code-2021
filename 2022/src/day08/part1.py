b4dd871b340835434e338673384f2585bbdeadfc
nuruvilu <nuruvilu@konpaku.tech>
2022-12-08 22:49:53 -0600

    2022 day 8 (BAD)

--- 2022/src/day08/part1.py
+++ 2022/src/day08/part1.py
@@ -10,10 +10,46 @@ from functools import reduce
 import common as aoc
 
 
+def vis(grid, visible):
+    for i, row in enumerate(grid):
+        for j, n in enumerate(row):
+            print(n if (i, j) in visible else '.', end='')
+        print()
+
+
 def solve(inp):
-    pass
+    grid = [[int(n) for n in row] for row in inp]
+    pivot_grid = [[row[i] for row in grid] for i, _ in enumerate(grid[0])]
+
+    visible = set()
+    for i, row in enumerate(grid):
+        last = -1
+        for j, n in enumerate(row):
+            if n > last:
+                last = n
+                visible.add((i, j))
+    for i, row in enumerate(grid):
+        last = -1
+        for j, n in enumerate(row[::-1]):
+            if n > last:
+                last = n
+                visible.add((i, len(row) - j - 1))
+    for i, row in enumerate(pivot_grid):
+        last = -1
+        for j, n in enumerate(row):
+            if n > last:
+                last = n
+                visible.add((j, i))
+    for i, row in enumerate(pivot_grid):
+        last = -1
+        for j, n in enumerate(row[::-1]):
+            if n > last:
+                last = n
+                visible.add((len(row) - j - 1, i))
+    #vis(grid, visible)
+    return len(visible)
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
--- 2022/src/day08/part2.py
+++ 2022/src/day08/part2.py
@@ -11,9 +11,52 @@ import common as aoc
 
 
 def solve(inp):
-    pass
+    grid = [[int(n) for n in row] for row in inp]
+
+    tops = 0
+    for i, row in enumerate(grid):
+        for j, n in enumerate(row):
+            curr = 1
+            #print(i, j, n)
+            for k in range(i - 1, -1, -1):
+                if grid[k][j] >= n:
+                    #print(i, k)
+                    curr *= i - k
+                    break
+            else:
+                curr *= i
+           #print(curr)
+            for k in range(i + 1, len(grid)):
+                if grid[k][j] >= n:
+                    #print(i, k)
+                    curr *= k - i
+                    break
+            else:
+                curr *= len(grid) - i - 1
+            #print(curr)
+            for k in range(j - 1, -1, -1):
+                if grid[i][k] >= n:
+                    #print(i, k)
+                    curr *= j - k
+                    break
+            else:
+                curr *= j
+            #print(curr)
+            for k in range(j + 1, len(row)):
+                if grid[i][k] >= n:
+                    #print(i, k)
+                    curr *= k - j
+                    break
+            else:
+                curr *= len(row) - j - 1
+            if curr > tops:
+                tops = curr
+            #print(curr)
+            #print()
+    #vis(grid, visible)
+    return tops
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
