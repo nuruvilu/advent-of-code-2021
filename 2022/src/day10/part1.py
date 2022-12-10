f23fbce7d374980426a81b380fc6e5ce41f2b864
nuruvilu <nuruvilu@konpaku.tech>
2022-12-10 01:00:52 -0600

    2022 day 10

--- 2022/src/day10/part1.py
+++ 2022/src/day10/part1.py
@@ -11,9 +11,26 @@ import common as aoc
 
 
 def solve(inp):
-    pass
+    cycle = 0
+    x = 1
+    points = 0
+    nextp = 20
+    for op in inp:
+        hold = 0
+        if op == 'noop':
+            cycle += 1
+        else:
+            _, num = op.split()
+            hold = int(num)
+            cycle += 2
+        if nextp <= 220 and cycle >= nextp:
+            tcycle = (cycle // 10) * 10
+            points += tcycle * x
+            nextp += 40
+        x += hold
+    return points
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
--- 2022/src/day10/part2.py
+++ 2022/src/day10/part2.py
@@ -10,10 +10,35 @@ from functools import reduce
 import common as aoc
 
 
+def pixelate(pixels, x, i):
+    xp = x + 40 * (i // 40)
+    if i in (xp - 1, xp, xp + 1):
+        pixels[i] = '█'
+
+
+def render(pixels):
+    window = aoc.partition(pixels, 40)
+    return '\n'.join(''.join(row) for row in window)
+
+
 def solve(inp):
-    pass
+    cycle = -1
+    x = 1
+    pixels = [' '] * 240
+    for op in inp:
+        if op == 'noop':
+            cycle += 1
+            pixelate(pixels, x, cycle)
+        else:
+            _, num = op.split()
+            cycle += 1
+            pixelate(pixels, x, cycle)
+            cycle += 1
+            pixelate(pixels, x, cycle)
+            x += int(num)
+    return render(pixels)
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
