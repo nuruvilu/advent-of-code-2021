b4f10aad0ad8dc3bdba20bd387dbefc260dfeeac
nuruvilu <nuruvilu@konpaku.tech>
2022-12-12 00:01:22 -0600

    2022 day 12

--- 2022/src/day12/part1.py
+++ 2022/src/day12/part1.py
@@ -10,10 +10,38 @@ from functools import reduce
 import common as aoc
 
 
+grid = None
+
+
+def is_target(x):
+    return grid[x[0]][x[1]] == 'E'
+
+
+def get_next(x):
+    c, d = x.item
+    nexts = []
+    curr_height = ord('a') if grid[c][d] == 'S' else ord(grid[c][d])
+    for a, b in aoc.adjs(x.item):
+        if aoc.inbounds2(a, b, len(grid), len(grid[0])):
+            height = ord('z') if grid[a][b] == 'E' else ord(grid[a][b])
+            if height - curr_height <= 1:
+                nexts.append((a, b))
+    return nexts
+
+
 def solve(inp):
-    pass
+    start = None
+    global grid
+    grid = inp
+    for i, row in enumerate(inp):
+        for j, e in enumerate(row):
+            if e == 'S':
+                start = (i, j)
+                break
+    result = aoc.bfs(start, get_next, is_target)
+    return len(result.path)
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readgrid('input.txt')
     print(solve(inp))
--- 2022/src/day12/part2.py
+++ 2022/src/day12/part2.py
@@ -10,10 +10,41 @@ from functools import reduce
 import common as aoc
 
 
+grid = None
+
+
+def is_target(x):
+    return grid[x[0]][x[1]] == 'E'
+
+
+def get_next(x):
+    c, d = x.item
+    nexts = []
+    curr_height = ord('a') if grid[c][d] == 'S' else ord(grid[c][d])
+    for a, b in aoc.adjs(x.item):
+        if aoc.inbounds2(a, b, len(grid), len(grid[0])):
+            height = ord('z') if grid[a][b] == 'E' else ord(grid[a][b])
+            if height - curr_height <= 1:
+                nexts.append((a, b))
+    return nexts
+
+
 def solve(inp):
-    pass
+    global grid
+    grid = inp
+    mini = 999999
+    for i, row in enumerate(inp):
+        for j, e in enumerate(row):
+            if e in ('S', 'a'):
+                try:
+                    result = aoc.bfs((i, j), get_next, is_target)
+                except:
+                    continue
+                curr_len = len(result.path)
+                mini = min(mini, curr_len)
+    return mini
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readgrid('input.txt')
     print(solve(inp))
