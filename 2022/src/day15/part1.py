800e3b4a461dd906d22bebde1aea4e757a2d0fb5
nuruvilu <nuruvilu@konpaku.tech>
2022-12-15 01:11:06 -0600

    2022 day 15 :^)

--- 2022/src/day15/part1.py
+++ 2022/src/day15/part1.py
@@ -1,19 +1,46 @@
 import math
 import operator
+import re
 
 import sys
 sys.path.append('..')
 
 from heapq import heappush, heappop
 from functools import reduce
+from collections import defaultdict
 
 import common as aoc
 
 
+EMPTY = 0
+BEACON = 1
+NO_BEACON = 2
+
+
 def solve(inp):
-    pass
+    marked = defaultdict(int)
+    line_num = 2000000
+    def_not_beacons = 0
+    for line in inp:
+        groups = re.match(
+            r'Sensor at x=(-{0,1}\d+), y=(-{0,1}\d+): closest beacon'
+            r' is at x=(-{0,1}\d+), y=(-{0,1}\d+)',
+            line
+        ).groups()
+        groups = [int(x) for x in groups]
+        sensor, beacon = [tuple(x) for x in aoc.partition(groups, 2)]
+        target = aoc.mandist(sensor, beacon)
+        marked[beacon] = BEACON
+        i = 0
+        while aoc.mandist((sensor[0] + i, line_num), sensor) <= target:
+            for p in [(sensor[0] + i, line_num), (sensor[0] - i, line_num)]:
+                if marked[p] == EMPTY:
+                    marked[p] = NO_BEACON
+                    def_not_beacons += 1
+            i += 1
+    return def_not_beacons
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
--- 2022/src/day15/part2.py
+++ 2022/src/day15/part2.py
@@ -1,19 +1,46 @@
 import math
 import operator
+import re
 
 import sys
 sys.path.append('..')
 
 from heapq import heappush, heappop
 from functools import reduce
+from collections import defaultdict
 
 import common as aoc
 
 
-def solve(inp):
-    pass
+def solve(inp):  # go and grab yourself a drink :^)
+    cap = 4_000_000
+    possible = set()
+    sensors = []
+    for sensor_num, line in enumerate(inp):
+        groups = re.match(
+            r'Sensor at x=(-{0,1}\d+), y=(-{0,1}\d+): closest beacon'
+            r' is at x=(-{0,1}\d+), y=(-{0,1}\d+)',
+            line
+        ).groups()
+        groups = [int(x) for x in groups]
+        sensor, beacon = [tuple(x) for x in aoc.partition(groups, 2)]
+        target = aoc.mandist(sensor, beacon)
+        sensors.append((sensor, target))
+        for i in range(target):
+            for p in [(sensor[0] + i, sensor[1] - target + i + 1),
+                      (sensor[0] + target - i + 1, sensor[1] - i),
+                      (sensor[0] - i, sensor[1] + target - i + 1),
+                      (sensor[0] - target + i + 1, sensor[1] + i)]:
+                if 0 <= p[0] <= cap and 0 <= p[1] <= cap:
+                    possible.add(p)
+        print(sensor_num)
+    for sensor, target in sensors:
+        print(sensor)
+        possible = set(p for p in possible if aoc.mandist(p, sensor) > target)
+    x, y = list(possible)[0]
+    return x * 4_000_000 + y
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
