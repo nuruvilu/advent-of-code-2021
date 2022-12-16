ea9a241664d1dcd007cdd864b02cbd251fca1988
nuruvilu <nuruvilu@konpaku.tech>
2022-12-16 02:18:15 -0600

    2022 day 16

--- 2022/src/day16/part1.py
+++ 2022/src/day16/part1.py
@@ -1,5 +1,6 @@
 import math
 import operator
+import re
 
 import sys
 sys.path.append('..')
@@ -10,10 +11,61 @@ from functools import reduce
 import common as aoc
 
 
-def solve(inp):
-    pass
+def solve(inp):  # SLOW
+    valves = {}
+    worthwhile_cnt = 0
+    for line in inp:
+        head = re.match(r'Valve ([a-zA-Z]{2}) has flow rate=(\d+);.+', line)
+        name, rate = head.groups()
+        rate = int(rate)
+        if 'valves' in line:
+            tunnels = line.split(' valves ')[1].split(', ')
+        else:
+            tunnels = line.split(' valve ')[1].split(', ')
+        valves[name] = (rate, tunnels)
+        if rate > 0:
+            worthwhile_cnt += 1
+    pressure = [-1]
+    progress = [30]
+    path = [None]
+
+    def step(curr):
+        mins_left, curr_pressure, turned, pos = curr.item
+        if mins_left < progress[0]:
+            print(mins_left)
+            progress[0] = mins_left
+        if mins_left == 0 or len(turned) // 2 >= worthwhile_cnt:
+            if curr_pressure > pressure[0]:
+                pressure[0] = curr_pressure
+                path[0] = curr.path + [curr.item]
+            return []
+        elif pos not in turned and valves[pos][0] > 0:
+            turned_p = turned + pos
+            return [(
+                mins_left - 1,
+                curr_pressure + (mins_left - 1) * valves[pos][0],
+                turned_p,
+                pos
+            )]
+        else:
+            res = []
+            for tunnel in valves[pos][1]:
+                res.append((
+                    mins_left - 1,
+                    curr_pressure,
+                    turned,
+                    tunnel
+                ))
+            return res
+    
+    try:
+        aoc.bfs((30, 0, '', "AA"), step, lambda _: False)
+    except RuntimeError:
+        print('done!')
+    
+    return pressure[0]
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
--- 2022/src/day16/part2.py
+++ 2022/src/day16/part2.py
@@ -1,5 +1,6 @@
 import math
 import operator
+import re
 
 import sys
 sys.path.append('..')
@@ -10,10 +11,58 @@ from functools import reduce
 import common as aoc
 
 
+def release_pressure(valves, turned):
+    total = 0
+    for t in turned:
+        total += valves[t][0]
+    return total
+
+
 def solve(inp):
-    pass
+    valves = {}
+    worthwhile_cnt = 0
+    for line in inp:
+        head = re.match(r'Valve ([a-zA-Z]{2}) has flow rate=(\d+);.+', line)
+        name, rate = head.groups()
+        rate = int(rate)
+        if 'valves' in line:
+            tunnels = line.split(' valves ')[1].split(', ')
+        else:
+            tunnels = line.split(' valve ')[1].split(', ')
+        valves[name] = (rate, tunnels)
+        if rate > 0:
+            worthwhile_cnt += 1
+    states = [(0, set(), 'AA', 'AA')]
+    for i in range(26):
+        print('mins:', i)
+        new_states = []
+        for pressure, turned, me, ele in states:
+            if len(turned) // 2 >= worthwhile_cnt:
+                new_states.append((
+                    release_pressure(valves, turned) + pressure,
+                    turned,
+                    me,
+                    ele
+                ))
+                continue
+            mes = [(v, turned) for v in valves[me][1]]
+            eles = [(v, turned) for v in valves[ele][1]]
+            if me not in turned and valves[me][0] > 0:
+                mes.append((me, turned | {me}))
+            if ele not in turned and valves[ele][0] > 0:
+                eles.append((ele, turned | {ele}))
+            for me2, me_t in mes:
+                for ele2, ele_t in eles:
+                    new_states.append((
+                        release_pressure(valves, turned) + pressure,
+                        turned | me_t | ele_t,
+                        me2,
+                        ele2
+                    ))
+        states = sorted(new_states, key=lambda t: t[0], reverse=True)[:8_000]
+    return states[0]
 
 
 if __name__ == '__main__':
-    inp = aoc.readnums('input.txt')
+    inp = aoc.readlines('input.txt')
     print(solve(inp))
