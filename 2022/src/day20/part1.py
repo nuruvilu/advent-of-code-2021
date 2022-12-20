6e01f2b1931271dff135fdf4a8f920019375aaa7
nuruvilu <nuruvilu@konpaku.tech>
2022-12-20 00:08:55 -0600

    2022 day 20

--- 2022/src/day20/part1.py
+++ 2022/src/day20/part1.py
@@ -11,7 +11,24 @@ import common as aoc
 
 
 def solve(inp):
-    pass
+    for _ in range(len(inp)):
+        i = 0
+        while isinstance(inp[i], tuple):
+            i += 1
+        val = inp.pop(i)
+        j = (i + val) % len(inp)
+        if j == 0:
+            inp.append((val, True))
+        else:
+            inp.insert(j, (val, True))
+        # print(inp)
+    s = 0
+    # print(inp)
+    zero = inp.index((0, True))
+    for n in (1000, 2000, 3000):
+        s += inp[(zero + n) % len(inp)][0]
+        print(inp[(zero + n) % len(inp)])
+    return s
 
 
 if __name__ == '__main__':
--- 2022/src/day20/part2.py
+++ 2022/src/day20/part2.py
@@ -11,7 +11,36 @@ import common as aoc
 
 
 def solve(inp):
-    pass
+    key = 811589153
+    inp = [key * n for n in inp]
+    for k in range(len(inp)):
+        i = 0
+        while isinstance(inp[i], tuple):
+            i += 1
+        val = inp.pop(i)
+        j = (i + val) % len(inp)
+        if j == 0 and val != 0:
+            inp.append((val, k))
+        else:
+            inp.insert(j, (val, k))
+    for time in range(2, 11):
+        print(time)
+        for curr in range(len(inp)):
+            i = 0
+            while inp[i][-1] != curr:
+                i += 1
+            val, k = inp.pop(i)
+            j = (i + val) % len(inp)
+            if j == 0 and val != 0:
+                inp.append((val, k))
+            else:
+                inp.insert(j, (val, k))
+    s = 0
+    zero = [i for i, (val, _) in enumerate(inp) if val == 0][0]
+    for n in (1000, 2000, 3000):
+        s += inp[(zero + n) % len(inp)][0]
+        print(inp[(zero + n) % len(inp)])
+    return s
 
 
 if __name__ == '__main__':
