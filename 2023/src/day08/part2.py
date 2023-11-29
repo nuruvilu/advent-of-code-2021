ae5654fdd1c15638a2297b5f28aa469f1b668437
nuruvilu <nuruvilu@konpaku.tech>
2023-11-29 08:52:31 -0600

    2023 set up

---
+++ 2023/src/day08/part2.py
@@ -0,0 +1,22 @@
+import math
+import operator
+
+import sys
+sys.path.append('..')
+
+from heapq import heappush, heappop
+from functools import reduce
+from pathlib import Path
+
+import common as aoc
+from aocd import get_data
+
+
+def solve(inp):
+    return None
+
+
+if __name__ == '__main__':
+    # sample = aoc.read(Path('sample.txt'))
+    inp = aoc.read(get_data(day=8, year=2023))
+    print(solve(inp))
