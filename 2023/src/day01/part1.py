e3b9097759367b6c1dfacd845e5d7585362b03d4
nuruvilu <nuruvilu@konpaku.tech>
2023-12-01 13:51:06 -0600

    2023 day 1

--- 2023/src/day01/part1.py
+++ 2023/src/day01/part1.py
@@ -9,14 +9,26 @@ from functools import reduce
 from pathlib import Path
 
 import common as aoc
-from aocd import get_data
+from aocd import get_data, submit
 
 
 def solve(inp):
-    return None
+    n = 0
+    for line in inp:
+        z = ''
+        for ch in line:
+            if ch.isdigit():
+                if not z:
+                    z = ch + ch
+                else:
+                    z = z[0] + ch
+        n += int(z)
+    return n
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=1, year=2023))
-    print(solve(inp))
+    #inp = aoc.readlines(Path('sample.txt'))
+    inp = aoc.readlines(get_data(day=1, year=2023))
+    answer = solve(inp)
+    print(answer)
+    submit(answer)
--- 2023/src/day01/part2.py
+++ 2023/src/day01/part2.py
@@ -9,14 +9,53 @@ from functools import reduce
 from pathlib import Path
 
 import common as aoc
-from aocd import get_data
+from aocd import get_data, submit
 
+nums = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+]
 
 def solve(inp):
-    return None
+    n = 0
+    for line in inp:
+        zz = {}
+        for num in nums:
+            x = line.find(num)
+            if x != -1:
+                zz[x] = num
+            x = line.rfind(num)
+            if x != -1:
+                zz[x] = num
+        z = ''
+        for i, ch in enumerate(line):
+            if i in zz:
+                d = nums.index(zz[i]) + 1
+                print(d, zz[i], i)
+                if not z:
+                    z = str(d) + str(d)
+                else:
+                    z = z[0] + str(d)
+            if ch.isdigit():
+                if not z:
+                    z = ch + ch
+                else:
+                    z = z[0] + ch
+        print(zz, z, line)
+        n += int(z)
+    return n
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=1, year=2023))
-    print(solve(inp))
+    #inp = aoc.readlines(Path('sample.txt'))
+    inp = aoc.readlines(get_data(day=1, year=2023))
+    answer = solve(inp)
+    print(answer)
+    #submit(answer)
