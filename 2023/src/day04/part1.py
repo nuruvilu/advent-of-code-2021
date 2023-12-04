30a5bb78961fdb619262dd29fc7bb90a8004005e
nuruvilu <nuruvilu@konpaku.tech>
2023-12-03 23:47:35 -0600

    2023 day 4

--- 2023/src/day04/part1.py
+++ 2023/src/day04/part1.py
@@ -13,10 +13,23 @@ from aocd import get_data
 
 
 def solve(inp):
-    return None
+    total = 0
+    for line in inp:
+        points = 0
+        _, numbers = line.split(':')
+        winners, drawn = numbers.split('|')
+        winners = set(winners.strip().split())
+        for x in drawn.strip().split():
+            if x in winners:
+                if points:
+                    points *= 2
+                else:
+                    points = 1
+        total += points
+    return total
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=4, year=2023))
+    #inp = aoc.readlines(Path('sample.txt'))
+    inp = aoc.readlines(get_data(day=4, year=2023))
     print(solve(inp))
--- 2023/src/day04/part2.py
+++ 2023/src/day04/part2.py
@@ -7,16 +7,25 @@ sys.path.append('..')
 from heapq import heappush, heappop
 from functools import reduce
 from pathlib import Path
+from collections import defaultdict
 
 import common as aoc
 from aocd import get_data
 
 
 def solve(inp):
-    return None
+    instances = defaultdict(int)
+    for i, line in enumerate(inp, start=1):
+        _, numbers = line.split(':')
+        winners, drawn = numbers.split('|')
+        winners = set(winners.strip().split())
+        count = len([x for x in drawn.strip().split() if x in winners])
+        for j in range(i + 1, min(len(inp), i + count) + 1):
+            instances[j] += instances[i] + 1
+    return len(inp) + sum(instances.values())
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=4, year=2023))
+    #inp = aoc.readlines(Path('sample.txt'))
+    inp = aoc.readlines(get_data(day=4, year=2023))
     print(solve(inp))
