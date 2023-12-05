de0b965e8bb6f491db1082fcf87d5c94e9d1ba18
nuruvilu <nuruvilu@konpaku.tech>
2023-12-05 00:38:59 -0600

    2023 day 5

--- 2023/src/day05/part1.py
+++ 2023/src/day05/part1.py
@@ -13,10 +13,27 @@ from aocd import get_data
 
 
 def solve(inp):
-    return None
+    _, seeds = inp[0][0].split(':')
+    seeds = [int(s) for s in seeds.strip().split()]
+    almanac = []
+    loc = 9e10
+    for mapping in inp[1:]:
+        almanac.append([])
+        for row in mapping[1:]:
+            almanac[-1].append(tuple(map(int, row.split())))
+    for seed in seeds:
+        x = seed
+        for step in almanac:
+            done = False
+            for dest, src, rng in step:
+                if not done and x >= src and x <= src + rng:
+                    x = dest + (x - src)
+                    done = True
+        loc = min(x, loc)
+    return loc
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=5, year=2023))
+    #inp = aoc.readstanzas(Path('sample.txt'))
+    inp = aoc.readstanzas(get_data(day=5, year=2023))
     print(solve(inp))
--- 2023/src/day05/part2.py
+++ 2023/src/day05/part2.py
@@ -6,17 +6,51 @@ sys.path.append('..')
 
 from heapq import heappush, heappop
 from functools import reduce
+from multiprocessing import Pool  # :^)
 from pathlib import Path
 
 import common as aoc
 from aocd import get_data
 
 
+almanac = []
+
+
+def handle_seeds(seed_pair):
+    loc = 9e10
+    start, rrr = seed_pair
+    for seed in range(start, start + rrr):
+        if seed % 100_000 == 0:
+            print(((seed - start) / rrr) * 100)
+        x = seed
+        for step in almanac:
+            done = False
+            for dest, src, rng in step:
+                if not done and x >= src and x <= src + rng:
+                    x = dest + (x - src)
+                    done = True
+        loc = min(x, loc)
+    return loc
+
+
 def solve(inp):
-    return None
+    _, seeds = inp[0][0].split(':')
+    seeds = aoc.partition([int(s) for s in seeds.strip().split()], 2)
+    for mapping in inp[1:]:
+        almanac.append([])
+        for row in mapping[1:]:
+            almanac[-1].append(tuple(map(int, row.split())))
+    new_seeds = []
+    for start, rrr in seeds:
+        r4 = rrr // 4
+        for n in range(4):
+            new_seeds.append((start + r4 * n, rrr - r4 * (n - 1)))
+    with Pool(len(seeds)) as p:
+        results = p.map(handle_seeds, seeds)
+        return min(results)
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=5, year=2023))
+    #inp = aoc.readstanzas(Path('sample.txt'))
+    inp = aoc.readstanzas(get_data(day=5, year=2023))
     print(solve(inp))
