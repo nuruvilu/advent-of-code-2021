63c0eb2e870a2f912c29f4cdb2171146c7ecde0d
nuruvilu <nuruvilu@konpaku.tech>
2023-12-13 00:09:57 -0600

    2023 day 13

--- 2023/src/day13/part1.py
+++ 2023/src/day13/part1.py
@@ -12,11 +12,39 @@ import common as aoc
 from aocd import get_data
 
 
+def test(grid):
+    for j in range(1, len(grid[0])):
+        #print(grid, j)
+        for row in grid:
+            left, right = row[:j], row[j:]
+            #if j == 5:
+                #print(left, right)
+            if len(left) > len(right) and not left[::-1].startswith(right):
+                break
+            elif len(left) <= len(right) and not right[::-1].endswith(left):
+                break
+        else:
+            return j
+            break
+    return 0
+
+
 def solve(inp):
-    return None
+    total = 0
+    for grid in inp:
+        res = test([''.join(x) for x in grid])
+        if res != 0:
+            total += res
+            continue
+        new_grid = [['.'] * len(grid) for _ in range(len(grid[0]))]
+        for i, row in enumerate(grid):
+            for j, item in enumerate(row):
+                new_grid[j][i] = item
+        total += 100*test([''.join(x) for x in new_grid])
+    return total
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=13, year=2023))
+    #inp = aoc.readstanzas(Path('sample.txt'))
+    inp = aoc.readstanzas(get_data(day=13, year=2023))
     print(solve(inp))
--- 2023/src/day13/part2.py
+++ 2023/src/day13/part2.py
@@ -4,6 +4,7 @@ import operator
 import sys
 sys.path.append('..')
 
+from collections import defaultdict
 from heapq import heappush, heappop
 from functools import reduce
 from pathlib import Path
@@ -12,11 +13,48 @@ import common as aoc
 from aocd import get_data
 
 
+def get_badlines(grid):
+    badlines = defaultdict(int)
+    for j in range(1, len(grid[0])):
+        counted = False
+        for i, row in enumerate(grid):
+            left, right = row[:j], row[j:]
+            smudges = 0
+            for k, (l, r) in enumerate(zip(left[::-1], right)):
+                if l != r:
+                    smudges += 1
+                    if smudges > 1:
+                        break
+            if smudges < 1:
+                continue
+            elif smudges < 2 and not counted:
+                counted = True
+            else:
+                break
+        else:
+            if counted:
+                badlines[j] += 1
+    return badlines
+
+
 def solve(inp):
-    return None
+    total = 0
+    for grid in inp:
+        badlines = get_badlines([''.join(x) for x in grid])
+        res = [j for j, lines in badlines.items() if lines == 1]
+        if res:
+            total += res[0]
+        else:
+            new_grid = [['.'] * len(grid) for _ in range(len(grid[0]))]
+            for i, row in enumerate(grid):
+                for j, item in enumerate(row):
+                    new_grid[j][i] = item
+            badlines = get_badlines([''.join(x) for x in new_grid])
+            total += 100*[j for j, lines in badlines.items() if lines == 1][0]
+    return total
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=13, year=2023))
+    #inp = aoc.readstanzas(Path('sample.txt'))
+    inp = aoc.readstanzas(get_data(day=13, year=2023))
     print(solve(inp))
