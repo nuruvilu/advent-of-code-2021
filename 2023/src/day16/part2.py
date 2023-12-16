1c170824f9abfbac3f589a61b37a59cf1919d264
nuruvilu <nuruvilu@konpaku.tech>
2023-12-15 23:22:57 -0600

    2023 day 16

--- 2023/src/day16/part2.py
+++ 2023/src/day16/part2.py
@@ -13,10 +13,51 @@ from aocd import get_data
 
 
 def solve(inp):
-    return None
+    entrypoints = [((0, i), 'R') for i in range(len(inp))]
+    entrypoints += [((len(inp[0]) - 1, i), 'L') for i in range(len(inp))]
+    entrypoints += [((i, len(inp) - 1), 'D') for i in range(len(inp[0]))]
+    entrypoints += [((i, 0), 'U') for i in range(len(inp[0]))]
+    maxeng = 0
+    for i, entrypoint in enumerate(entrypoints):
+        print(i / len(entrypoints))
+        beams = [entrypoint]
+        energized = set()
+        visited = set()
+        while beams:
+            (curr_x, curr_y), curr_dir = beams.pop(0)
+            while aoc.inbounds2(curr_y, curr_x, len(inp), len(inp[0])):
+                if ((curr_x, curr_y), curr_dir) in visited:
+                    break
+                visited.add(((curr_x, curr_y), curr_dir))
+                energized.add((curr_x, curr_y))
+                if inp[curr_y][curr_x] == '/':
+                    if curr_dir == 'U': curr_dir = 'L'
+                    elif curr_dir == 'L': curr_dir = 'U'
+                    elif curr_dir == 'D': curr_dir = 'R'
+                    elif curr_dir == 'R': curr_dir = 'D'
+                elif inp[curr_y][curr_x] == '\\':
+                    if curr_dir == 'U': curr_dir = 'R'
+                    elif curr_dir == 'R': curr_dir = 'U'
+                    elif curr_dir == 'D': curr_dir = 'L'
+                    elif curr_dir == 'L': curr_dir = 'D'
+                elif inp[curr_y][curr_x] == '-':
+                    if curr_dir in ('U', 'D'):
+                        curr_dir = 'L'
+                        other = 'R'
+                        other_pos = aoc.udlr_xy(other, (curr_x, curr_y))
+                        beams.append((other_pos, other))
+                elif inp[curr_y][curr_x] == '|':
+                    if curr_dir in ('L', 'R'):
+                        curr_dir = 'U'
+                        other = 'D'
+                        other_pos = aoc.udlr_xy(other, (curr_x, curr_y))
+                        beams.append((other_pos, other))
+                curr_x, curr_y = aoc.udlr_xy(curr_dir, (curr_x, curr_y))
+        maxeng = max(maxeng, len(energized))
+    return maxeng
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=16, year=2023))
+    #inp = aoc.readgrid(Path('sample.txt'))
+    inp = aoc.readgrid(get_data(day=16, year=2023))
     print(solve(inp))
