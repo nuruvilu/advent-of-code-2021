eaa363a9948c0450a4f313a13013a1a2ca091d75
nuruvilu <nuruvilu@konpaku.tech>
2023-12-21 22:58:18 -0600

    2023 day 21

--- 2023/src/day21/part1.py
+++ 2023/src/day21/part1.py
@@ -12,11 +12,33 @@ import common as aoc
 from aocd import get_data
 
 
+MAX_STEPS = 64
+
+
 def solve(inp):
-    return None
+    total = [0]
+    def step(p_item):
+        loc, steps = p_item.item
+        nexts = []
+        for i, j in aoc.adjs(loc):
+            if aoc.inbounds2(i, j, len(inp), len(inp[0])) and inp[i][j] != '#':
+                nexts.append(((i, j), steps + 1))
+        return nexts
+    def is_target(p_item):
+        steps = p_item.item[1]
+        if steps == MAX_STEPS:
+            total[0] += 1
+        return steps > MAX_STEPS
+    start = None
+    for i, row in enumerate(inp):
+        for j, x in enumerate(row):
+            if x == 'S':
+                start = (i, j)
+    aoc.bfs((start, 0), step, is_target)
+    return total[0]
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=21, year=2023))
+    #inp = aoc.readgrid(Path('sample.txt'))
+    inp = aoc.readgrid(get_data(day=21, year=2023))
     print(solve(inp))
--- 2023/src/day21/part2.py
+++ 2023/src/day21/part2.py
@@ -4,6 +4,7 @@ import operator
 import sys
 sys.path.append('..')
 
+from collections import defaultdict, deque
 from heapq import heappush, heappop
 from functools import reduce
 from pathlib import Path
@@ -12,11 +13,34 @@ import common as aoc
 from aocd import get_data
 
 
+MAX_STEPS = 26501365
+
+
 def solve(inp):
-    return None
+    width = len(inp)
+    start = None
+    for i, row in enumerate(inp):
+        for j, x in enumerate(row):
+            if x == 'S':
+                start = (i, j)
+    coeffs = [0]
+    it = 0
+    queue = {start}
+    while len(coeffs) < 4:
+        it += 1
+        new_queue = set()
+        for loc in queue:
+            for i, j in aoc.adjs(loc):
+                if inp[i % width][j % width] != '#':
+                    new_queue.add((i, j))
+        if it % width == MAX_STEPS % width:
+            coeffs.append(len(new_queue))
+        queue = new_queue
+    tiles = MAX_STEPS // width
+    return coeffs[1] + (coeffs[2] - coeffs[1]) * tiles + (tiles * (tiles - 1) // 2) * (coeffs[3] - 2*coeffs[2] + coeffs[1])
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=21, year=2023))
+    #inp = aoc.readgrid(Path('sample.txt'))
+    inp = aoc.readgrid(get_data(day=21, year=2023))
     print(solve(inp))
