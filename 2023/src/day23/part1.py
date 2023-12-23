14a25a9637d83526282efaf0b664238cb118acb2
nuruvilu <nuruvilu@konpaku.tech>
2023-12-23 02:40:37 -0600

    JUST

--- 2023/src/day23/part1.py
+++ 2023/src/day23/part1.py
@@ -4,6 +4,7 @@ import operator
 import sys
 sys.path.append('..')
 
+from collections import deque
 from heapq import heappush, heappop
 from functools import reduce
 from pathlib import Path
@@ -13,10 +14,51 @@ from aocd import get_data
 
 
 def solve(inp):
-    return None
+    start = None
+    stop = None
+    for j, item in enumerate(inp[0]):
+        if item == '.':
+            start = (0, j)
+    for j, item in enumerate(inp[-1]):
+        if item == '.':
+            stop = (len(inp) - 1, j)
+    #print(start, stop)
+    def step(curr):
+        nexts = []
+        for (i, j) in aoc.adjs(curr):
+            if aoc.inbounds2(i, j, len(inp), len(inp[0])) and inp[i][j] != '#':
+                #if inp[curr[0]][curr[1]] == 'v':
+                    #print(curr, i, j)
+                if inp[curr[0]][curr[1]] == '.':
+                    nexts.append((i, j))
+                elif inp[curr[0]][curr[1]] == '>' and j == curr[1] + 1:
+                    nexts.append((i, j))
+                elif inp[curr[0]][curr[1]] == '<' and j == curr[1] - 1:
+                    nexts.append((i, j))
+                elif inp[curr[0]][curr[1]] == '^' and i == curr[0] - 1:
+                    nexts.append((i, j))
+                elif inp[curr[0]][curr[1]] == 'v' and i == curr[0] + 1:
+                    nexts.append((i, j))
+        #print(nexts)
+        return nexts
+    def is_target(p_item):
+        return p_item == stop
+    q = deque([(start, set())])
+    longest = 0
+    while q:
+        pos, visited = q.popleft()
+        #print(pos)
+        if is_target(pos):
+            longest = max(longest, len(visited))
+        else:
+            for p in step(pos):
+                if p not in visited:
+                    q.append((p, visited | {pos}))
+    #print(start, stop)
+    return longest
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=23, year=2023))
+    #inp = aoc.readgrid(Path('sample.txt'))
+    inp = aoc.readgrid(get_data(day=23, year=2023))
     print(solve(inp))
--- 2023/src/day23/part2.py
+++ 2023/src/day23/part2.py
@@ -4,19 +4,68 @@ import operator
 import sys
 sys.path.append('..')
 
-from heapq import heappush, heappop
-from functools import reduce
+from collections import deque
+from dataclasses import dataclass, field
+from heapq import heappush, heappushpop
+from functools import reduce, lru_cache
 from pathlib import Path
 
 import common as aoc
 from aocd import get_data
 
 
+@dataclass(order=True)
+class Fork:
+    steps: int
+    pos: (int, int)=field(compare=False)
+    seen: {(int, int)}=field(compare=False)
+
+
 def solve(inp):
-    return None
+    start = None
+    stop = None
+    for j, item in enumerate(inp[0]):
+        if item == '.':
+            start = (0, j)
+    for j, item in enumerate(inp[-1]):
+        if item == '.':
+            stop = (len(inp) - 1, j)
+
+    forks = [Fork(0, start, set())]
+    #passed_over = []
+    cache = {}
+    pots = []
+    MAX_FORKS = 5000
+    while forks:
+        print(forks[-1].steps)
+        #print([len(s) for p, s in forks])
+        if len(forks) > MAX_FORKS:
+            forks = sorted(forks, key=lambda f: len(f[1]), reverse=True)[:MAX_FORKS]
+        new_forks = []
+        for fork in forks:
+            #print(len(seen))
+            if fork.pos == stop:
+                #print([x for x, _ in forks])
+                pots.append(len(seen))
+            nexts = [fork]
+            while len(nexts) == 1:
+                cfork = nexts.pop()
+                for (i, j) in aoc.adjs(cfork.pos):
+                    if (i, j) == stop:
+                        pots.append(len(cfork.seen) + 1)
+                    if aoc.inbounds2(i, j, len(inp), len(inp[0])) and inp[i][j] != '#' and (i, j) not in cfork.seen:
+                        nexts.append(Fork(cfork.steps + 1, (i, j), cfork.seen | {cfork.pos}))
+            for nfork in nexts:
+                if len(new_forks) < MAX_FORKS:
+                    heappush(new_forks, nfork)
+                else:
+                    heappushpop(new_forks, nfork)
+        forks = new_forks
+    print(pots)
+    return max(pots)
 
 
 if __name__ == '__main__':
-    # sample = aoc.read(Path('sample.txt'))
-    inp = aoc.read(get_data(day=23, year=2023))
+    #inp = aoc.readgrid(Path('sample.txt'))
+    inp = aoc.readgrid(get_data(day=23, year=2023))
     print(solve(inp))
